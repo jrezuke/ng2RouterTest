fbe21fb3b9a8cd0c9e9d7db05e36f375a5c5d378
jrezuke <j.rezuke@verizon.net>
2016-08-05 16:45:00 -0400

    added breadcrumbs sortof

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ROUTER_DIRECTIVES, Router} from '@angular/router';
+import { ROUTER_DIRECTIVES, Router, NavigationStart} from '@angular/router';
 import { SecurityService } from './security/security.service'
 @Component({
     selector: 'my-app',
@@ -8,11 +8,32 @@ import { SecurityService } from './security/security.service'
 
 })
 export class AppComponent implements OnInit {
+    previousPath: string;
+    currentPath: string;
 
-
-    constructor(private _security: SecurityService) { }
+    constructor(private _security: SecurityService, private _router: Router) { }
 
     ngOnInit() {
-    
+        this._router.events.subscribe(event => {
+            if (event instanceof NavigationStart) {
+                console.log("event:", event);
+                this.onPathChange(event.url);
+            }
+        });
+
+    }
+
+    onPathChange(path: string) {
+
+        if (this.currentPath) {
+            this.previousPath = this.currentPath;
+        }
+        this.currentPath = path;
+        if (this._security.IsAuthorized) {
+
+        }
+        else {
+
+        }
     }
 }
